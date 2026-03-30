b27d1672ba586e5e0aacb4d82197e234ec0a67a5
Kelll31 <46720761+Kelll31@users.noreply.github.com>
2026-03-30 20:23:38 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: '/dianel_furs_redesign/',
+    base: '/Dianel_Furs_Redesign/',
     build: {
       outDir: 'dist',
       emptyOutDir: true,
