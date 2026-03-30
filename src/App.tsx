0d1f33ade197dafd32a06a788b66e81b1f189a26
Kelll31 <46720761+Kelll31@users.noreply.github.com>
2026-03-30 20:26:54 +0300

    some fixes

--- src/App.tsx
+++ src/App.tsx
@@ -94,7 +94,7 @@ export default function App() {
   return (
     <HelmetProvider>
       <AppProvider>
-        <Router>
+        <Router basename={import.meta.env.BASE_URL}>
           <AppContent />
         </Router>
       </AppProvider>
